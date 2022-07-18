c54b0178029b241b2f35488e8e9edf2ae359c089
robert-winn-ai <104718630+robert-winn-ai@users.noreply.github.com>
2022-07-18 15:11:27 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,8 +20,8 @@ for option in options:
     chrome_options.add_argument(option)
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
-#driver.get('http://nytimes.com')
 driver.get('https://storage.googleapis.com/qa__test/Elements/index.html')
-print(driver.title)
+SearchResults = driver.find_element_by_xpath("/html/body/div[1]/header/h1/a/span")
+#driver.get('http://nytimes.com')
+print(SearchResults.title)
 
