b1e58915dcaf33ed9e9c21cf92df86b40d6cae4c
robert-winn-ai <104718630+robert-winn-ai@users.noreply.github.com>
2022-07-19 10:22:01 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
